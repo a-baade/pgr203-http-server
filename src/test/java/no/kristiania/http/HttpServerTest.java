5ed75963a3b942db89756ca5d5dad5e48ebb1da8
a-baade <andreasbaade@hotmail.com>
2021-09-21 17:20:14 +0200

    Mod maven.yml

--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -7,10 +7,12 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
+
     @Test
     void shouldReturn404ForUnknownRequestTarget() throws IOException {
         HttpServer server = new HttpServer(1001);
         HttpClient client = new HttpClient("localHost",1001,"/non-existing");
         assertEquals(404,client.getStatusCode());
     }
+
 }
