e2c81b457015f3e77d10122616570c862b28de48
a-baade <andreasbaade@hotmail.com>
2021-09-21 17:22:13 +0200

    shouldReturn404ForUnknownRequestTarget(✅)

--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -10,8 +10,8 @@ public class HttpServerTest {
 
     @Test
     void shouldReturn404ForUnknownRequestTarget() throws IOException {
-        HttpServer server = new HttpServer(1001);
-        HttpClient client = new HttpClient("localHost",1001,"/non-existing");
+        HttpServer server = new HttpServer(10001);
+        HttpClient client = new HttpClient("localHost",10001,"/non-existing");
         assertEquals(404,client.getStatusCode());
     }
 
